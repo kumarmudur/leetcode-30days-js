c55718858a0699ae363339bc0543c2d7387250ec
kumarmudur <kumarmudur10@gmail.com>
2023-09-23 17:53:03 -0400

    [JS] closure examples

--- closures/fun.js
+++ closures/fun.js
@@ -2,5 +2,17 @@ const createHelloWorld = () => {
     return function (...args) {
         return 'Hello World';
     }
-};  
+}; 
+
+// Arrow functions
+const createHelloWorld1 = () => {
+    return () => 'Hello World';
+}
+
+// Arrow Syntax + Rest Arguments
+const createHelloWorld2 = () => {
+    return (...args) => 'Hello World';
+}
+
+
 

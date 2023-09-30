d8509b481574f5235609adf87faef41989fc797f
kumarmudur <kumarmudur10@gmail.com>
2023-09-29 20:41:06 -0400

    [Closure] example

--- closures/counter.ts
+++ closures/counter.ts
@@ -8,11 +8,23 @@ const createCounter = (n: number) => {
 }
 
 // Solution 2: Postfix Increment Syntax
-const createCounter2 = function(n: number) {
+const createCounter2 = (n: number) => {
     return function() {
       return n++;      
     };
 };
 
+// Solution 3: Prefix Decrement and Increment Syntax
+const createCounter3 = (n: number) => {
+    --n;
+    return function() {
+      return ++n;      
+    };
+};
+
+// Solution 4: Postfix Increment Syntax With Arrow Function
+const createCounter4 = (n: number) => {
+    return () => n++;
+};
 
 export {};

6a101f9e8f103dfe817b13fd718cd6f15ae25d16
kumarmudur <kumarmudur10@gmail.com>
2023-10-02 20:59:48 -0400

    [Functions] factory example

---
+++ functions/fun.js
@@ -0,0 +1,13 @@
+// Approach 1: Object Factory
+const expect = function(val) {
+    return {
+      toBe: (val2) => {
+        if(val !== val2) throw new Error("Not Equal");
+        return true;
+      },
+      notToBe: (val2) => {
+        if(val === val2) throw new Error("Equal");
+        return true;
+      }
+    };
+};
